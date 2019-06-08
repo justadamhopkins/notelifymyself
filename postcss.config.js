08ad9776475dd75a930babdf0b12e6cdaaa0ddd9
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-06-08 18:05:32 +0100

    added custom media query helpers

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('cssnext'),
     require('postcss-url'),
+    require('postcss-fontpath'),
     require('autoprefixer'),
     require('postcss-preset-env')({
       stage: 0
