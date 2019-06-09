82dbca484d977a550d1aff80335b8d93ba7df380
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-06-09 16:25:23 +0100

    fixed css modules class name rendering in enzyme jest

--- src/components/navigation/header/header.spec.js
+++ src/components/navigation/header/header.spec.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { Header } from '.'
+import styles from './styles.css'
 
 describe('<Header/>', () => {
   it('Should render component', () => {
