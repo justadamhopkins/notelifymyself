0de25e01200a04d60925983e2a39e7fd17271171
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-06-09 13:59:49 +0100

    started layout unit tests

--- src/containers/layout/layout.spec.js
+++ src/containers/layout/layout.spec.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { Layout } from '.'
-import * as styles from '../../components/navigation/sideDraw/hamburger'
+import styles from '../../components/navigation/sideDraw/hamburger/styles.css'
 
 describe('<Layout/>', () => {
-  const props = { sideDrawVisible: true, sideDrawToggle: jest.fn() }
+  const props = { sideDrawVisible: false, sideDrawToggle: () => {} }
   const layout = mount(<Layout { ...props }/>)
   it('Should render component', () => {
     expect(layout).toMatchSnapshot()
   })
-  it('initializes the sidedrawvisible state as false', () => {
-    console.log(layout.debug())
-    // expect(layout.state().sideDrawVisible).toBe(false)
+  it('initializes the sidedrawvisible state as false and sidedraw toggle as a func', () => {
+    expect(layout.props().sideDrawVisible).toEqual(false)
   })
 })
