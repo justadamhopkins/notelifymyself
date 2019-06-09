15ef57bc516cf98ef4a13676555d0f871b46eb96
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-06-09 08:35:19 +0100

    updated snapshot and starting getting css modules working with jest

--- src/containers/layout/layout.spec.js
+++ src/containers/layout/layout.spec.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import { Layout } from '.'
+import * as styles from '../../components/navigation/sideDraw/hamburger'
 
 describe('<Layout/>', () => {
+  const props = { sideDrawVisible: true, sideDrawToggle: jest.fn() }
+  const layout = mount(<Layout { ...props }/>)
   it('Should render component', () => {
-    const wrapper = shallow(<Layout/>)
-    expect(wrapper).toMatchSnapshot()
+    expect(layout).toMatchSnapshot()
+  })
+  it('initializes the sidedrawvisible state as false', () => {
+    console.log(layout.debug())
+    // expect(layout.state().sideDrawVisible).toBe(false)
   })
 })
