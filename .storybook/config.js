a98ab16264b28a12ab8fe71ad6ce8384f4fe3c17
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-07-04 16:49:38 +0100

    first story toggle hamburger

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,25 @@
-import { configure, addDecorator } from '@storybook/react'
+import { configure, addDecorator, addParameters } from '@storybook/react'
 import centered from '@storybook/addon-centered/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { themes } from '@storybook/theming';
 
 function loadStories() {
   console.log('Loading stories .........');
   const req = require.context('../src', true, /\.stories\.js$/)
   req.keys().forEach(filename => req(filename))
 }
+
+addParameters({
+  options: {
+    isFullscreen: false,
+    showNav: true,
+    showPanel: true,
+    panelPosition: 'bottom',
+    enableShortcuts: true,
+    isToolshown: true,
+    theme: themes.dark,
+  },
+});
 
 
 
--- src/components/navigation/sidedraw/hamburger/hamburger.stories.js
+++ src/components/navigation/sidedraw/hamburger/hamburger.stories.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { withKnobs } from '@storybook/addon-knobs'
+import { withKnobs, boolean } from '@storybook/addon-knobs'
 import { Hamburger } from '.'
 
 
-storiesOf('Hamburger', module)
-  .addDecorator(withKnobs)
-  .add('open', () => (<Hamburger
-    toggleSideDraw={() => {}}
-    animateHamBurger={false}/>))
+const stories = storiesOf('Hamburger', module);
+
+stories.addDecorator(withKnobs)
+
+stories.add('toggle', () => (<Hamburger
+    animateHamBurger={boolean('close', false)}/>))
