2fe7c4b9b829ee17bffc02d2e3269329721ed3ea
Adam Hopkins <adamhopkins87@gmail.com>
2019-08-11 18:42:47 +0100

    updated postcss for font magician fonts

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,25 @@
+const magician = require('postcss-font-magician')
+
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('cssnext'),
     require('postcss-url'),
-    require('postcss-fontpath'),
     require('autoprefixer'),
     require('postcss-preset-env')({
       stage: 0
+    }),
+    magician({
+      variants: {
+        Rubik: {
+          300: [],
+          400: [],
+          500: [],
+          700: [],
+          900: []
+        }
+      },
+      foundries: ['google']
     })
   ]
 }
