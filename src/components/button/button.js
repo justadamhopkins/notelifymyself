4e75f0f625a5cb49ce3ab080c921a81d10220c28
Adam Hopkins <adamhopkins87@gmail.com>
2019-09-08 17:39:48 +0100

    created button component

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,10 +1,16 @@
 import React from 'react'
-// import styles from './styles.css'
+import PropTypes from 'prop-types'
+import styles from './styles.css'
 
-const Button = () => {
+const Button = ({ buttonState, buttonCopy }) => {
   return (
-    <button>submit</button>
+    <button className={[styles.Button, styles[buttonState]].join(' ')}>{buttonCopy}</button>
   )
+}
+
+Button.propTypes = {
+  buttonState: PropTypes.string,
+  buttonCopy: PropTypes.string
 }
 
 export default Button
