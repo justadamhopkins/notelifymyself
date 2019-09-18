369c7a112a9548874d270dd22ffd42e2e713bba3
Adam Hopkins <adamhopkins87@gmail.com>
2019-09-18 21:36:28 +0100

    changed file names

--- server/controllers/mockCtrl.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  test(req, res) {
-    const name = req.query.name || 'World'
-    res.setHeader('Content-Type', 'application/json')
-    res.send(JSON.stringify({ greeting: `Hello ${name}!` }))
-  }
-}
