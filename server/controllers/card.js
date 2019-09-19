d88986d6830d35364f5b17d4d10c8da251367acf
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-09-19 09:15:25 +0100

    tidy logic on server

--- server/controllers/card.js
+++ server/controllers/card.js
@@ -7,11 +7,11 @@ const Card = model('Card', cardSchema)
 module.exports = {
   saveCard: async (request, response) => {
     try {
-      const cards = new Card(request.body);
-      const result = await cards.save();
-      response.send(result);
+      const cards = new Card(request.body)
+      const result = await cards.save()
+      response.send(result)
     } catch (error) {
-      response.status(500).send(error);
-      }
+      response.status(500).send(error)
     }
   }
+}
