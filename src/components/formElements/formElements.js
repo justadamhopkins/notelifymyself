ca092af606f03c1d60fd6ce98cd9f5a185626a42
Adam Hopkins <adamhopkins87@gmail.com>
2019-09-20 22:56:56 +0100

    added prop type and changed data on form

--- src/components/formElements/formElements.js
+++ src/components/formElements/formElements.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Form, Field } from 'react-final-form'
 import { Input } from './input'
 import { Select } from './select'
@@ -6,31 +7,32 @@ import { TextArea } from './textarea'
 import { Button } from '../button'
 import styles from './styles.css'
 const options = ['animals', 'tech', 'people', 'nature']
-const FormElements = () => {
+const FormElements = ({ send }) => {
   return (
     <Form
-      onSubmit={() => {}}
-      validate={(values) => {
-        const errors = {}
-        if (!values.email) {
-          errors.email = 'Please enter a valid email address.'
-        }
-        if (!values.password) {
-          errors.password = 'Please enter a password of at least 6 characters'
-        }
-        return errors
+      onSubmit={send}
+      initialValues={{ pictures: 'animals' }}
+      validate={() => { // pass values
+        // const errors = {}
+        // if (!values.email) {
+        //   errors.email = 'Please enter a valid email address.'
+        // }
+        // if (!values.password) {
+        //   errors.password = 'Please enter a password of at least 6 characters'
+        // }
+        // return errors
       }}>
     {({ handleSubmit }) => (
       <form className={styles.Form} onSubmit={handleSubmit}>
         <div className={styles.LeftBlock}>
         <Field
           component={Input}
-          name={'Title'}
+          name={'title'}
           type={'text'}
           label={'Card Title'}/>
         <Field
           component={Input}
-          name={'Title subtitle'}
+          name={'subtitle'}
           type={'text'}
           label={'Card subtitle'}/>
         </div>
@@ -38,22 +40,27 @@ const FormElements = () => {
         <Field
           component={Select}
           label={'Picture Type'}
-          name={'Title'}
+          name={'pictures'}
           ops={options}/>
         <Field
           component={TextArea}
           label={'Note'}
-          name={'Title'}
+          name={'note'}
           rows={'4'}
           cols={'20'}/>
         </div>
         <Button
+          type={'submit'}
           buttonState={'active'}
           buttonCopy={'Submit'}/>
       </form>
     )}
   </Form>
   )
+}
+
+FormElements.propTypes = {
+  send: PropTypes.Func
 }
 
 export default FormElements
