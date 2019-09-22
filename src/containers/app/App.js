a3bb4143bdce736e1da251ca9dbb5c5b5dc8e9e7
Adam Hopkins <adamhopkins87@gmail.com>
2019-09-22 17:36:39 +0100

    removed fetch

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -3,14 +3,12 @@ import { hot } from 'react-hot-loader'
 import { Layout } from '../layout'
 import { Cards } from '../cards'
 import { Form } from '../form'
-import Fetch from '../fetch'
 
 const App = () => (
   <div>
     <Layout>
       <Form/>
       <Cards/>
-      <Fetch/>
     </Layout>
   </div>
 )
