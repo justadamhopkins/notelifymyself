4d36c28bbe7557f3eb28a320829b59418f0eea89
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-09-25 21:44:39 +0100

    updated card image with dynamic prop

--- src/components/card/cardImage/cardImage.js
+++ src/components/card/cardImage/cardImage.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styles from './styles.css'
 
-const CardImage = () => {
+const CardImage = ({ pictures }) => {
   return (
     <div className={styles.CardImage}>
-      <img src="https://placeimg.com/300/200/animals"/>
+      <img src={`https://placeimg.com/300/200/${pictures}`}/>
     </div>
   )
+}
+
+CardImage.propTypes = {
+  pictures: PropTypes.string
 }
 
 export default CardImage
