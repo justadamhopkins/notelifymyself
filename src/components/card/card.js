658c6be0e8e3699c44dd87b8dc40c7f1fa6d4622
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-09-25 21:44:23 +0100

    added logic to card to dynamically create data

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,25 +1,33 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styles from './styles.css'
 import { CardTitle } from './cardTitle'
 import { CardImage } from './cardImage'
 import { CardPara } from './cardPara'
-const Card = () => {
+const Card = ({ card }) => {
+  if(!card) return false
   return (
     <div className={styles.Card}>
       <ul>
-        <li className={styles.cardWrap}>
-          <CardTitle />
-          <CardImage />
-          <CardPara />
+      {Object.keys(card).map((id) => {
+        return (
+          <li className={styles.cardWrap} key={id}>
+            <CardTitle
+              title={card[id].title}
+              subtitle={card[id].subtitle}
+              pictures={card[id].pictures}/>
+            <CardImage pictures={card[id].pictures}/>
+            <CardPara note={card[id].note}/>
         </li>
-        <li className={styles.cardWrap}>
-          <CardTitle />
-          <CardImage />
-          <CardPara />
-        </li>
+        )
+      })}
       </ul>
     </div>
   )
+}
+
+Card.propTypes = {
+  card: PropTypes.object
 }
 
 export default Card
