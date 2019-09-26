0ac1ab069fdfbc5da450070c33bba200dfc5354e
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-09-26 21:29:56 +0100

    added functionality for remove card to the component

--- src/containers/cards/cards.js
+++ src/containers/cards/cards.js
@@ -2,25 +2,36 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Card } from '../../components/card'
+import { removeCard } from '../../store/actions/card/cardActions'
 
 export class Cards extends Component {
+  deleteCard = (event) => {
+    const { parentElement } = event.target
+    const { id } = parentElement.parentElement.dataset
+    const { removeCard } = this.props
+    removeCard(id)
+  }
+
   render() {
     const { cards } = this.props
     return (
     <div>
-      <Card card={cards}/>
+      <Card
+      card={cards}
+      deletedCard={this.deleteCard}/>
     </div>
     )
   }
 }
 
 Cards.propTypes = {
-  cards: PropTypes.object
+  cards: PropTypes.object,
+  removeCard: PropTypes.func
 }
 
 export default connect(
   (state) => ({
     cards: state.card
   }),
-  { }
+  { removeCard }
 )(Cards)
