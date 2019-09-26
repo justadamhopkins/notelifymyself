a3ac42a916a1fc2756e77971f2f8957bb1d42a94
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-09-26 21:27:11 +0100

    updated card test for reducer

--- src/store/reducers/card/card.spec.js
+++ src/store/reducers/card/card.spec.js
@@ -1,20 +1,32 @@
-import { addCard } from './card'
+import { addCard, removeCard } from './card'
 
 describe('@addCard', () => {
   describe('when adding a card', () => {
-    const mockState = {}
-    const action = {
-      type: 'ADD_CARD',
-      payload: {
-        title: 'Mr',
-        subtitle: 'Adam',
-        _id: '123'
-      }
-    }
     it('should add the component to the store', () => {
+      const mockState = {}
+      const action = {
+        type: 'ADD_CARD',
+        payload: {
+          title: 'Mr',
+          subtitle: 'Adam',
+          _id: '123'
+        }
+      }
       const addMyCard = addCard(mockState, action)
       const expected = { 123: { title: 'Mr', subtitle: 'Adam', _id: '123' } }
       expect(addMyCard).toEqual(expected)
+    })
+    it('should add the component to the store', () => {
+      const mockState = { 123: { title: 'Mr', subtitle: 'Adam', _id: '123' } }
+      const action = {
+        type: 'REMOVE_CARD',
+        payload: {
+          id: '123'
+        }
+      }
+      const removeMyCard = removeCard(mockState, action)
+      const expected = {}
+      expect(removeMyCard).toEqual(expected)
     })
   })
 })
