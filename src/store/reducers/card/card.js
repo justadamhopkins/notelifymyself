6452e107971889724bb28c79c6b763dbd0ae19ad
Adam Hopkins <Adam.Hopkins@arcadiagroup.co.uk>
2019-09-26 21:27:56 +0100

    added remove card reducer function

--- src/store/reducers/card/card.js
+++ src/store/reducers/card/card.js
@@ -2,3 +2,10 @@ export const addCard = (state, action) => {
   const { payload } = action
   return { ...state, [payload._id]: { ...payload } }
 }
+
+export const removeCard = (state, action) => {
+  const { payload } = action
+  const stateCopy = Object.assign({}, state)
+  delete stateCopy[payload.id]
+  return stateCopy
+}
