8a18d5881ad22b71cd3e079da8737b550ec64925
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-01 22:05:15 +0100

    fixed tests

--- src/components/navigation/sidedraw/sidedraw.spec.js
+++ src/components/navigation/sidedraw/sidedraw.spec.js
@@ -3,12 +3,16 @@ import { mount } from 'enzyme'
 import { Sidedraw } from '.'
 
 describe('@Sidedraw', () => {
+  const props = {
+    addCategory: jest.fn(),
+    isVisible: false
+  }
   it('should render the component', () => {
-    const sidebar = mount(<Sidedraw isVisible={false}/>)
+    const sidebar = mount(<Sidedraw {...props}/>)
     expect(sidebar.length).toBe(1)
   })
   it('should render the navigation items inside the sidebar', () => {
-    const sidebar = mount(<Sidedraw isVisible={false}/>)
+    const sidebar = mount(<Sidedraw {...props}/>)
     expect(sidebar.find('NavigationItems').exists()).toBe(true)
   })
 })
