7b998cc4e8623a760b64388ee1b1a36a410ee0a7
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-05 18:55:01 +0100

    added new actions to card container

--- src/containers/cards/cards.js
+++ src/containers/cards/cards.js
@@ -2,23 +2,14 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Card } from '../../components/card'
-import { removeCard } from '../../store/actions/card/cardActions'
+import { removeCardEle } from '../../store/actions/card/cardActions'
 
 export class Cards extends Component {
-  deleteCard = async (event) => {
+  deleteCard = (event) => {
     const { parentElement } = event.target
     const { id } = parentElement.parentElement.dataset
-    const { removeCard } = this.props
-    removeCard(id)
-    const response = await fetch(process.env.baseUrl + '/removeCard', {
-      method: 'POST',
-      body: JSON.stringify({ id }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    const data = await response.json()
-    return data
+    const { removeCardEle } = this.props
+    removeCardEle(id)
   }
 
   render() {
@@ -35,12 +26,12 @@ export class Cards extends Component {
 
 Cards.propTypes = {
   cards: PropTypes.object,
-  removeCard: PropTypes.func
+  removeCardEle: PropTypes.func
 }
 
 export default connect(
   (state) => ({
     cards: state.card
   }),
-  { removeCard }
+  { removeCardEle }
 )(Cards)
