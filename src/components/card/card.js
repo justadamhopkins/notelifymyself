df9ecee06e362699b89c4109a65a26477620115e
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-05 22:19:06 +0100

    refactor card component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,28 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './styles.css'
-import { CardTitle } from './cardTitle'
-import { CardImage } from './cardImage'
-import { CardPara } from './cardPara'
-import { CloseIcon } from '../closeIcon'
+import { CardList } from './cardList'
 const Card = ({ card, deletedCard }) => {
-  if(!card) return false
+  if (!card) return false
   return (
     <div className={styles.Card}>
       <ul>
-      {Object.keys(card).map((id) => {
-        return (
-          <li className={styles.cardWrap} key={id} data-id={id}>
-            <CloseIcon deleteCard={deletedCard}/>
-            <CardTitle
+        {Object.keys(card).map(id => {
+          return (
+            <CardList
+              key={id}
+              id={id}
+              deletedCard={deletedCard}
               title={card[id].title}
               subtitle={card[id].subtitle}
-              pictures={card[id].pictures}/>
-            <CardImage pictures={card[id].pictures}/>
-            <CardPara note={card[id].note}/>
-        </li>
-        )
-      })}
+              pictures={card[id].pictures}
+              note={card[id].note}
+            />
+          )
+        })}
       </ul>
     </div>
   )
