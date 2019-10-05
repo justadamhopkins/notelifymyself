122c6902467ca1c52b31920b4bf0d2ef2de2ffea
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-05 18:54:51 +0100

    added new actions to form container

--- src/containers/form/form.js
+++ src/containers/form/form.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { addCard } from '../../store/actions/card/cardActions'
+import { addCardEle } from '../../store/actions/card/cardActions'
 import { FormElements } from '../../components/formElements'
 
 export class Form extends Component {
-  submit = async (values) => { // todo write test
-    const { addCard } = this.props
-    const response = await fetch(process.env.baseUrl + '/addCard', {
-      method: 'POST',
-      body: JSON.stringify(values),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    const data = await response.json()
-    addCard(data)
+  submit = async (values) => {
+    const { addCardEle } = this.props
+    addCardEle(values)
   }
 
   render() {
@@ -30,10 +22,10 @@ export class Form extends Component {
 }
 
 Form.propTypes = {
-  addCard: PropTypes.func
+  addCardEle: PropTypes.func
 }
 
 export default connect(
   () => ({}),
-  { addCard }
+  { addCardEle }
 )(Form)
