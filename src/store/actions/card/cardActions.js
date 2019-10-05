8e7cc87efe336a94435ee7c1b4d5b793ddeb82bf
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-05 18:54:04 +0100

    added add card and remove card as thunk async actions

--- src/store/actions/card/cardActions.js
+++ src/store/actions/card/cardActions.js
@@ -9,3 +9,29 @@ export const addCard = (card) => {
     payload: { ...card }
   }
 }
+
+export const addCardEle = (values) => {
+  return async dispatch => {
+    const response = await fetch(process.env.baseUrl + '/addCard', {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    dispatch(addCard(data))
+  }
+}
+
+export const removeCardEle = (id) => {
+  return async dispatch => {
+    const response = await fetch(process.env.baseUrl + '/removeCard', {
+      method: 'POST',
+      body: JSON.stringify({ id }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    const data = await response.json()
+    dispatch(removeCard(data._id))
+  }
+}
