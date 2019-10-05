00c85cade4b29f4a15b80a22ea5ebbab8195d197
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-05 18:52:22 +0100

    added redux thunk to store config

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const middlewares = []
+const middlewares = [thunk]
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)))
 
