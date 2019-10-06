18bad29e57de0e96005bce05e5c2b6ffaec81f51
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-06 17:00:22 +0100

    added layout changes + updated test

--- src/containers/layout/layout.js
+++ src/containers/layout/layout.js
@@ -1,34 +1,56 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { addCategory } from '../../store/actions/category'
 import { Sidedraw } from '../../components/navigation/sidedraw'
 import { Wrapper } from '../../components/wrapper'
 
-const Layout = ({ children }) => {
-  const [sideDrawVisible, setDrawState] = useState(false)
-
-  const sideDrawToggleHandler = () => {
-    setDrawState(!sideDrawVisible)
+export class Layout extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { sideDrawVisible: false }
   }
 
-  const addCategory = (vals) => {
-    console.log('TCL: addCategory -> vals', vals)
+  sideDrawToggleHandler = () => {
+    const { sideDrawVisible } = this.state
+    this.setState({ sideDrawVisible: !sideDrawVisible })
   }
 
-  return (
+  addCategory = (vals) => {
+    const { addCategory } = this.props
+    const _id = Math.random().toString().replace('0.', '')
+    const mergeId = { ...vals, _id }
+    addCategory(mergeId)
+  }
+
+  render() {
+    const { sideDrawVisible } = this.state
+    const { sideDrawToggleHandler, addCategory } = this
+    const { children, category } = this.props
+    return (
     <React.Fragment>
       <Sidedraw
         isVisible={sideDrawVisible}
-        addCategory={addCategory}/>
+        addCategory={addCategory}
+        categoryList={category}/>
       <Wrapper
         children={children}
         isVisible={sideDrawVisible}
         toggleSideDraw={sideDrawToggleHandler}/>
     </React.Fragment>
-  )
+    )
+  }
 }
 
 Layout.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  addCategory: PropTypes.func,
+  category: PropTypes.object
 }
 
-export default Layout
+export default connect(
+  (state) => ({
+    category: state.category
+  }),
+  { addCategory }
+)(Layout)
