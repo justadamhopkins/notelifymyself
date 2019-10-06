358507be7c5b1647e2eaf289af74d2e291b82d30
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-06 17:02:34 +0100

    started reach router app broken atm :-(

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
+import { Router } from '@reach/router'
 import { Layout } from '../layout'
 import { Cards } from '../cards'
 import { Form } from '../form'
 
-const App = () => (
-  <div>
-    <Layout>
-      <Form/>
-      <Cards/>
-    </Layout>
-  </div>
-)
+const App = () => {
+  return (
+    <div>
+      <Layout>
+        <Router>
+            <Form path="/category/:categoryId" />
+            <Cards path="/category/:categoryId"/>
+        </Router>
+      </Layout>
+    </div>
+  )
+}
+
 
 export default hot(module)(App)
