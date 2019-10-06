941a189a87e33151254d351d759df433d5ecf4e9
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-06 16:57:12 +0100

    added test for navigation item

--- src/components/navigation/navigationItems/navigationitems.spec.js
+++ src/components/navigation/navigationItems/navigationitems.spec.js
@@ -3,12 +3,15 @@ import { mount } from 'enzyme'
 import { NavigationItems } from '.'
 
 describe('@NavigationItems', () => {
+  const props = {
+    categoryList: { 111: { category: 'Nature', _id: 111 } }
+  }
   it('should render the component', () => {
-    const navigationWrapper = mount(<NavigationItems/>)
+    const navigationWrapper = mount(<NavigationItems {...props}/>)
     expect(navigationWrapper.length).toBe(1)
   })
   it('should render list items inside the navigation wrapper', () => {
-    const navigationWrapper = mount(<NavigationItems/>)
+    const navigationWrapper = mount(<NavigationItems {...props}/>)
     expect(navigationWrapper.find('NavigationItem').exists()).toBe(true)
   })
 })
