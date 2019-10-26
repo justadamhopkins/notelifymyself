2afffd0799a062d365da4f83677b96664afd58b3
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-26 19:39:50 +0100

    updated reducer to have ability to reset state

--- src/store/utils/reducerGen/reducerGen.js
+++ src/store/utils/reducerGen/reducerGen.js
@@ -1,10 +1,13 @@
-import { addCard, removeCard } from '../../reducers/card/card'
+import { addCard, removeCard } from '../../reducers/card'
+import { resetState } from '../../reducers/reset'
+
 
 const createReducers = (actionName = '') => {
   return function componentProperty(state = {}, action = {}) {
     switch (action.type) {
       case `ADD_${actionName}`: return addCard(state, action)
       case `REMOVE_${actionName}`: return removeCard(state, action)
+      case `RESET_${actionName}`: return resetState(action)
       default: return state
     }
   }
