c0f0bdd12b299643de76b79c6b7dc874008ea6b0
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-27 15:23:30 +0000

    added error object to text area

--- src/components/formElements/textarea/textarea.jsx
+++ src/components/formElements/textarea/textarea.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const TextArea = ({ rows, cols, label, input }) => {
+const TextArea = ({ rows, cols, label, input, meta }) => {
   const { onChange, name } = input
   return (
     <div>
       <label>{label}</label>
       <textarea onChange={onChange} name={name} placeholder={name} rows={rows} cols={cols}>
       </textarea>
+      {meta.error && meta.touched && <span className='error'>{meta.error}</span>}
     </div>
   )
 }
@@ -16,7 +17,8 @@ TextArea.propTypes = {
   rows: PropTypes.string,
   cols: PropTypes.string,
   label: PropTypes.string,
-  input: PropTypes.object
+  input: PropTypes.object,
+  meta: PropTypes.object
 }
 
 
