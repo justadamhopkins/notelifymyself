a3c32eae8b3bbbefb273b22a14d962200da2d8f5
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-29 21:39:55 +0000

    added add category route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,7 +1,9 @@
 const cardCtrl = require('../controllers/card')
+const categoryCtrl = require('../controllers/category')
 
 module.exports.routeList = (app) => {
   app.post('/api/addCard', cardCtrl.saveCard)
   app.post('/api/removeCard', cardCtrl.removeCard)
   app.post('/api/fetchCategory', cardCtrl.fetchCategory)
+  app.post('/api/addCategory', categoryCtrl.addCategory)
 }
