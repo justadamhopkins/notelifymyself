fc73c163fbedb51eddec6e58aba796e6ec3786c4
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-30 21:05:04 +0000

    changed server routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,8 +2,8 @@ const cardCtrl = require('../controllers/card')
 const categoryCtrl = require('../controllers/category')
 
 module.exports.routeList = (app) => {
-  app.post('/api/addCard', cardCtrl.saveCard)
-  app.post('/api/removeCard', cardCtrl.removeCard)
+  app.post('/api/cards/add', cardCtrl.saveCard)
+  app.post('/api/cards/remove', cardCtrl.removeCard)
   app.get('/api/category/:id', cardCtrl.fetchCategory)
-  app.post('/api/addCategory', categoryCtrl.addCategory)
+  app.post('/api/category', categoryCtrl.addCategory)
 }
