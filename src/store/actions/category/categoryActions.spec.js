784fdc003c62c0fa1ae0afd77d2dedc9cfccc021
Adam Hopkins <adamhopkins87@gmail.com>
2019-10-30 21:30:18 +0000

    added add category test

--- src/store/actions/category/categoryActions.spec.js
+++ src/store/actions/category/categoryActions.spec.js
@@ -1,5 +1,11 @@
-import { addCategory } from '.'
+import fetchMock from 'fetch-mock'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { addCategory, addCategoryEle } from '.'
 import { ADD_CATEGORY } from '../constants'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
 
 describe('@categoryActions', () => {
   it('should create an addCard action object', () => {
@@ -13,5 +19,31 @@ describe('@categoryActions', () => {
     }
     const result = addCategory(data)
     expect(result).toEqual(expected)
+  })
+  describe('@Category async actions', () => {
+    const OLD_ENV = process.env
+    process.env.baseUrl = 'http://localhost:3001/api'
+    beforeEach(() => {
+      process.env = { ...OLD_ENV }
+    })
+    afterEach(() => {
+      fetchMock.reset()
+    })
+    fit('adds category to store on successful response from server', async () => {
+      const catId = 'nature'
+      const store = mockStore({ category: { } })
+      fetchMock.post(process.env.baseUrl + '/category', {
+        body: JSON.stringify({ catId }),
+        headers: { 'content-type': 'application/json' }
+      })
+      const expectedActions = [
+        { type: ADD_CATEGORY, payload: { catId } }
+      ]
+
+      const thunk = addCategoryEle(catId)
+      await thunk(store.dispatch)
+
+      expect(store.getActions()).toEqual(expectedActions)
+    })
   })
 })
